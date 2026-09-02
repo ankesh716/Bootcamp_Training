3b8474d3079e08080ab40e732a7af4344064a6d4
Ankesh Kumar <Ankesh.kumar.cs27@iilm.edu>
2026-09-02 21:42:34 +0530

    Add solution for longest consecutive sequence problem

---
+++ Test/Q2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int n : nums)
+            set.add(n);
+
+        int ans = 0;
+
+        for (int n : set) {
+            if (!set.contains(n - 1)) {
+                int count = 1;
+
+                while (set.contains(n + count))
+                    count++;
+
+                ans = Math.max(ans, count);
+            }
+        }
+
+        return ans;
+    }
+}
